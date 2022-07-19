95f62892f4e7080a4916f03746ab5b671264676f
prudhviteja9499 <73590654+prudhviteja9499@users.noreply.github.com>
2022-07-19 11:52:07 +0530

    Add files via upload

---
+++ calci.java
@@ -0,0 +1,61 @@
+package slprogram;
+import java.util.Scanner;
+public class calci {
+
+	public static void main(String[] args) {
+		
+				    char operator;
+				    Double n0, n1, output;
+
+				    // create an object of Scanner class
+				    Scanner input = new Scanner(System.in);
+
+				    // ask users to enter operator
+				    System.out.println("Choose an operator: -, +, /, or *");
+				    operator = input.next().charAt(0);
+
+				    // ask users to enter numbers
+				    System.out.println("Enter first number");
+				    n0 = input.nextDouble();
+
+				    System.out.println("Enter second number");
+				    n1 = input.nextDouble();
+
+				    switch (operator) {
+
+				      // performs addition between numbers
+				      case '+':
+				        output = n0 + n1;
+				        System.out.println(n0 + " + " + n1 + " = " + output);
+				        break;
+
+				      // performs subtraction between numbers
+				      case '-':
+				        output = n0 - n1;
+				        System.out.println(n0 + " - " + n1 + " = " + output);
+				        break;
+
+				      // performs multiplication between numbers
+				      case '*':
+				        output = n0 * n1;
+				        System.out.println(n0 + " * " + n1 + " = " + output);
+				        break;
+
+				      // performs division between numbers
+				      case '/':
+				        output = n0 / n1;
+				        System.out.println(n0 + " / " + n1 + " = " + output);
+				        break;
+
+				      default:
+				        System.out.println("Invalid operator!");
+				        break;
+				    }
+
+				    input.close();
+				  }
+		}
+
+
+
+
